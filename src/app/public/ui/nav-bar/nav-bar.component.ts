8e72b19c7195bbedb20a69f11fe58ef479f0a4c4
ASSIST\Jaddie.Lorzano <jaddie.lorzano@assistrx.com>
2023-04-14 00:07:47 +0800

    update nav bar

--- src/app/public/ui/nav-bar/nav-bar.component.ts
+++ src/app/public/ui/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Navigation } from '../../interfaces/nav.model';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,5 +10,14 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+  @Input() content!: Navigation;
 
+  public get navMenu() { return this.content.menu };
+  public get link() { return this.content?.viewSlots};
+  public get logoUrl() { return this.content?.logo};
+  public showSpan: boolean = false;
+
+  onButtonClick(url?: string) {
+    if (url) window.location.href = url
+  }
 }
