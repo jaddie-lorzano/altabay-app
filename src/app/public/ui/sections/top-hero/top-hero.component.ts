8cc2f70f781d5247507d829bc02b3bb5b5f181f5
ASSIST\Jaddie.Lorzano <jaddie.lorzano@assistrx.com>
2023-04-19 03:42:28 +0800

    update top hero component

--- src/app/public/ui/sections/top-hero/top-hero.component.ts
+++ src/app/public/ui/sections/top-hero/top-hero.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopHeroContent } from 'src/app/public/interfaces/section.content.model';
 
 @Component({
   selector: 'section-top-hero',
@@ -9,5 +10,15 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./top-hero.component.scss']
 })
 export class TopHeroComponent {
+  @Input() content!: TopHeroContent;
 
+  public get id() { return this.content.id; }
+  public get title() { return this.content.title }
+  public get subtitle() { return this.content.subtitle }
+  public get button() { return this.content.button }
+  public get bgImage() { return `url(${this.content.bgImage})` }
+
+  onButtonClick(url: string) {
+    window.location.href = url;
+  }
 }
