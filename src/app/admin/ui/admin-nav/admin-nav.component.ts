3da75a6e3cf25e7b11679ec986981b60bd3ccf9d
ASSIST\Jaddie.Lorzano <jaddie.lorzano@assistrx.com>
2023-04-19 03:39:32 +0800

    add admin ui components

---
+++ src/app/admin/ui/admin-nav/admin-nav.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin-nav',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-nav.component.html',
+  styleUrls: ['./admin-nav.component.scss']
+})
+export class AdminNavComponent {
+
+}
