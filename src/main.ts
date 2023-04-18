236b5ea6c7d243a0837c9d1c03c9aec10ecfbb37
ASSIST\Jaddie.Lorzano <jaddie.lorzano@assistrx.com>
2023-04-19 03:35:38 +0800

    update index and main file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import APP_ROUTE from './app/app-route'
+import APP_ROUTE from './app/app-route';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core'
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(APP_ROUTE)
-  ]
+    provideRouter(APP_ROUTE),
+    importProvidersFrom(BrowserAnimationsModule, BrowserAnimationsModule)
+]
 })
