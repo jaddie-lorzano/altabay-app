e06dadf64d3b5512f5c48eba1e2866b909f27b5a
ASSIST\Jaddie.Lorzano <jaddie.lorzano@assistrx.com>
2023-04-19 03:40:35 +0800

    udpate home page

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopHeroComponent } from "../../ui/sections/top-hero/top-hero.component";
+import { ServicesComponent } from '../../ui/sections/services/services.component';
+import { OurPartnersComponent } from '../../ui/sections/our-partners/our-partners.component';
+import { CtaComponent } from '../../ui/sections/cta/cta.component';
+import { DataComponent } from '../../ui/sections/data/data.component';
+import { ContentService } from 'src/app/shared/data-access/content.service';
+import { TestimonialsComponent } from "../../ui/sections/testimonials/testimonials.component";
 
 @Component({
     selector: 'app-home',
@@ -9,9 +15,26 @@ import { TopHeroComponent } from "../../ui/sections/top-hero/top-hero.component"
     styleUrls: ['./home.component.scss'],
     imports: [
         CommonModule,
-        TopHeroComponent
+        CtaComponent,
+        DataComponent,
+        OurPartnersComponent,
+        ServicesComponent,
+        TopHeroComponent,
+        TestimonialsComponent
     ]
 })
 export class HomeComponent {
 
+  constructor (
+    private content: ContentService,
+  ) {}
+  private landingPage = this.content.landingPageContent;
+  public footer: string = '© 2023 by Altabay Solutions.';
+  public nav = this.content.navigationContent
+  public topHero = this.landingPage.topHero
+  public services = this.landingPage.services
+  public data = this.landingPage.data
+  public about = this.landingPage.about
+  public testimonials = this.landingPage.testimonials
+  public cta = this.landingPage.cta
 }
